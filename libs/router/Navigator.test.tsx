5089322e80e42e3708b271987ef7825c345abcc8
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-16 08:46:42 +0900

    Create Navigator test cases

---
+++ libs/router/Navigator.test.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import { Navigator } from 'router/Navigator';
+import { IRoute, Router } from 'router/Router';
+
+const sampleRoutes: IRoute[] = [{
+  path: '/hello',
+  title: 'Hello',
+  component: 'Hello',
+}, {
+  path: '/world',
+  title: 'World',
+  component: 'World',
+}];
+
+describe('Navigator', () => {
+  it('renders correctly with hello', () => {
+    const router: Router = new Router(sampleRoutes);
+
+    const tree: object = renderer
+      .create(
+        <Navigator
+          router={router}
+          path={'/hello'}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('renders correctly with world', () => {
+    const router: Router = new Router(sampleRoutes);
+
+    const tree: object = renderer
+      .create(
+        <Navigator
+          router={router}
+          path={'/world'}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
