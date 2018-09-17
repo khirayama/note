cc27ad108496af36b2f836f010e48eef19fae79f
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-09-17 15:03:27 +0900

    Update jest.config.e2e

--- jest.config.e2e.js
+++ jest.config.e2e.js
@@ -1,27 +1,9 @@
-module.exports = {
-  clearMocks: true,
-  coverageDirectory: "coverage",
-  globals: {
-    "ts-jest": {
-      "tsConfigFile": "tsconfig.json"
-    }
-  },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "json"
-  ],
-  testEnvironment: "node",
-  testMatch: [
-    "**/*.test.+(ts|tsx|js)"
-  ],
-  testPathIgnorePatterns: [
-    "/node_modules",
-    "/src",
-    "/libs"
-  ],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
-  },
-};
+const jestConfig = require('./jest.config');
+
+jestConfig.testPathIgnorePatterns = [
+  "/node_modules",
+  "/src",
+  "/libs"
+];
+
+module.exports = jestConfig;
